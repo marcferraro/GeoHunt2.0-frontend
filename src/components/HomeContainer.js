21873e9866872e5c0bc9401038b8378d6da9b18c
“Abbie <abbie.coghlan@gmail.com>
2021-05-26 14:22:07 -0500

    proxy

---
+++ src/components/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileCard from './ProfileCard'
+
+const HomeContainer = () => {
+
+    return (
+        <>
+            <ProfileCard></ProfileCard>
+        </>
+    )
+}
+
+export default HomeContainer
