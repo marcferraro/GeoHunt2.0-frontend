4c2365fe5fcab992e86bf9640473a4a016e36703
“Abbie <abbie.coghlan@gmail.com>
2021-05-27 19:43:35 -0500

    new user form

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import NewUserForm from './NewUserForm';
 import ProfileCard from './ProfileCard'
 
 
@@ -18,6 +19,7 @@ const HomeContainer = () => {
     return (
         <>
             <ProfileCard name={name}></ProfileCard>
+            <NewUserForm setName={setName}></NewUserForm>
         </>
     )
 }
