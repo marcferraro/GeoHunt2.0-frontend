221a8d5f1ab0901a036a26090240b198909721c0
“Abbie <abbie.coghlan@gmail.com>
2021-05-27 19:44:09 -0500

    new user form

---
+++ src/components/NewUserForm.js
@@ -0,0 +1,35 @@
+import React, { useState }  from 'react';
+
+const NewUserForm = ({setName}) => {
+    const [form, setForm] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        const reqObj = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(form)
+        }
+
+        fetch('/users', reqObj)
+        .then(resp => resp.json())
+        .then(user => {
+            console.log(user)
+            setName(user.name)
+        })
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type='text' value={form} onChange={(e) => setForm(e.target.value)}></input>
+                <input type='submit'></input>
+            </form> 
+        </>
+    )
+}
+
+export default NewUserForm
